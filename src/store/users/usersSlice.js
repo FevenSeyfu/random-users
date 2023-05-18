ef872fcaf0b68a794ceb0478d53868145ccc1442
FevenSeyfu <fevenseyfu00@gmail.com>
2023-05-18 19:49:04 +0300

    setup reducer

---
+++ src/store/users/usersSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  booksList,
+};
+
+const bookSlice = createSlice({
+  name: 'users',
+  initialState,
+
+});
+
+export default usersSlice.reducer;
